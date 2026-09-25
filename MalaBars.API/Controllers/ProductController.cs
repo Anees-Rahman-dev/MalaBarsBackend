9a64ec7c101f5ad0d31a316139cbbd1c63436e31
Anees-Rahman-dev <aneesrahman0101@gmail.com>
2026-09-25 19:15:43 +0530

    Installed Swagger

--- MalaBars.API/Controllers/ProductController.cs
+++ MalaBars.API/Controllers/ProductController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MalaBars.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MalaBars.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductController
+    public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+
+            return Ok(products);
+
+        }
     }
 }
